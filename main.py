f15a426a378b6b280f6adfb9d0a2a71082072b7b
Macreamy <117770872+Macreamy@users.noreply.github.com>
2022-11-08 20:21:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+user_prompt = "Type add, show, edit, complete or exit: "
+
+todos = []
+
+while True:
+    user_action = input(user_prompt)
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case "show" | "display":
+            for index, i in enumerate(todos):
+                i = i.title()
+                row = f"{index + 1}.{i}"
+                print(row)
+        case "edit":
+            number = int(input("Number of the todo item to edit: "))
+            number = number - 1
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo
+            print(new_todo)
+        case "exit":
+            break
+        case "complete":
+            number = int(input("Number of the todo item to complete: "))
+            completed = todos.pop(number - 1)
+            print("You have completed", completed)
+        case _:
+            print("Hey, unknown command")
+
+print("Bye!")
